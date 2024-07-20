b17f337f356b3ce6a36a1a2c8e63252b5a7389ca
Mr-Muneeb101 <160252527+Mr-Muneeb101@users.noreply.github.com>
2024-07-20 12:01:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,158 @@
+#! /usr/bin/env node
+import { faker } from "@faker-js/faker";
+import chalk from "chalk";
+import inquirer from "inquirer";
+// import inquirer, { ListQuestion } from "inquirer";
+// import { ChoiceCollection } from "inquirer";
+class Coustomer {
+    name;
+    gender;
+    phoneNumber;
+    accountnumber;
+    balance;
+    constructor(name, gender, phoneNumber, accountNumber, balance) {
+        this.name = name;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.accountnumber = accountNumber;
+        this.balance = balance;
+    }
+    tranaction(val) {
+        this.balance -= val;
+    }
+    deposit(val) {
+        this.balance += val;
+    }
+    viewBalance() {
+        console.log(chalk.whiteBright.bold(`\t\t Your Balance is : $${this.balance} \n`));
+    }
+    showinfo() {
+        console.log(chalk.white.italic(`Name:${this.name}\nGender: ${this.gender}\nPhoneNumber: ${this.phoneNumber}\nAccountNumber:${this.accountnumber}\nBalance${this.balance}`));
+    }
+}
+// interface account {
+//     accountNumber: number,
+//     balance: number;
+// }
+class bank {
+    Coustomers = [];
+    // accounts: account[] = [];
+    Transaction(id, ammonut) {
+        let Person = this.Coustomers.find((coustomer) => coustomer.accountnumber == id);
+        if (Person) {
+            Person.tranaction(ammonut);
+            console.log(chalk.blue.bold(`Your Transaction have been Done \n\t your current Ammount is ${chalk.white(`$${Person.balance}`)}`));
+        }
+        else {
+            console.log(chalk.red.italic(`cannot find an account`));
+        }
+    }
+    Deposit(id, ammonut) {
+        let Person = this.Coustomers.find((coustomer) => coustomer.accountnumber == id);
+        if (Person) {
+            Person.deposit(ammonut);
+            console.log(Person.balance);
+            console.log(chalk.blue.bold(`Your ammount have been Deposit \n\t your current Ammount is ${chalk.white(`$${Person.balance}`)}`));
+        }
+        else {
+            console.log(chalk.red.italic(`cannot find an account`));
+        }
+    }
+    viewBalance(id) {
+        let Person = this.Coustomers.find((coustomer) => coustomer.accountnumber == id);
+        if (Person) {
+            Person.viewBalance();
+        }
+        else {
+            console.log(chalk.red.italic(`cannot find an account`));
+        }
+    }
+    showinfo(id) {
+        let Person = this.Coustomers.find((coustomer) => coustomer.accountnumber == id);
+        if (Person) {
+            Person.showinfo();
+        }
+        else {
+            console.log(chalk.red.italic(`cannot find an account`));
+        }
+    }
+}
+let HBL = new bank();
+for (let i = 0; i < 3; i++) {
+    let P_firtname = faker.person.firstName();
+    let P_Gender = faker.person.sexType();
+    let P_number = parseInt('3' + faker.string.numeric(10));
+    let P_accountnumber = 1000 + i;
+    let P_balance = 1000 * i;
+    let newCoustomer = new Coustomer(P_firtname, P_Gender, P_number, P_accountnumber, P_balance);
+    HBL.Coustomers.push(newCoustomer);
+}
+;
+async function Bank_Operator() {
+    console.log(chalk.blue.bold(`\n\n\t\tWelcome to My \n\n`));
+    do {
+        let userInput = await inquirer.prompt([
+            {
+                name: "answer",
+                message: "Select operation",
+                type: "list",
+                choices: ["Trasaction", "Deposit", "ViewBalance", "MyInfo", chalk.red.italic("Exit")],
+            }
+        ]);
+        switch (userInput.answer) {
+            case "Trasaction":
+                let UserId = await inquirer.prompt([
+                    {
+                        name: "ID",
+                        message: "Enter your account number",
+                        type: "input",
+                    }, {
+                        name: "ammount",
+                        message: "Enter ammount",
+                        type: "input",
+                    }
+                ]);
+                HBL.Transaction(UserId.ID, parseInt(UserId.ammount));
+                break;
+            case "Deposit":
+                let account = await inquirer.prompt([
+                    {
+                        name: "ID",
+                        message: "Enter your account number",
+                        type: "input",
+                    }, {
+                        name: "ammount",
+                        message: "Enter ammount",
+                        type: "input",
+                    }
+                ]);
+                HBL.Deposit(account.ID, parseInt(account.ammount));
+                break;
+            case "ViewBalance":
+                let userInput = await inquirer.prompt([
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "please Enter your account Number",
+                    }
+                ]);
+                HBL.viewBalance(userInput.id);
+                break;
+            case "MyInfo":
+                let Input = await inquirer.prompt([
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "please Enter your account Number",
+                    }
+                ]);
+                HBL.showinfo(Input.id);
+                break;
+            case "Exit":
+                return false;
+            default:
+                break;
+        }
+    } while (true);
+}
+Bank_Operator();
